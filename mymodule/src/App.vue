40a60d8f2fd763b3d540ea2a049aeda46b6d7723
miyagi <zhanglei@infoearth.com>
2018-09-26 17:01:39 +0800

    页面

---
+++ mymodule/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <v-head />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Head from "@/components/Head.vue";
+
+export default {
+  name: "App",
+  components: {
+    "v-head": Head
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
